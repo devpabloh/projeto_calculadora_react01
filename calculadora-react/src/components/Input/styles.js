56d897beffea4ebb43de1c5f260661f3fbd911e8
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-25 15:50:37 -0300

    Update styles.js

--- calculadora-react/src/components/Input/styles.js
+++ calculadora-react/src/components/Input/styles.js
@@ -4,4 +4,11 @@ export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
     background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    Justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
 `

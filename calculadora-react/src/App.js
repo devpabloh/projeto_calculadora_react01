33d3c898ebd8e9c44f14305fbc0b56946b439812
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-26 14:59:59 -0300

    att

--- calculadora-react/src/App.js
+++ calculadora-react/src/App.js
@@ -1,35 +1,72 @@
 import Input from "./components/Input"
 import Button from "./components/Button"
-import {Container, Content, Row, Column} from "./styles";
+import {Container, Content, Row} from "./styles";
+
+/* Estados react */
+import {useState} from "react"
 
 const  App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('')
+  const handleOnClear = ()=> {
+    setCurrentNumber("0")
+    setFirstNumber("0")  
+    setOperation("")
+  }
+
+  const handleAddNumber = (num)=>{
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev} ${num}`)
+  }
+
+  const handleSomNumbers = ()=>{
+
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('*')
+    }else{
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation("")
+    }
+  }
+
+  const handleEquals = ()=>{
+
+    if(!firstNumber === '0' && operation === ""){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+  }
+}
+
   return (
     <Container>
       <Content>
-        <Input/>
+        <Input value={currentNumber}/>
         <Row>
-        <Button label="C"/>
-        <Button label="/"/>
-        <Button label="*"/>
+        <Button label="C" onClick={handleOnClear}/>
+        <Button label="/" onClick={() => handleAddNumber("/")}/>
+        <Button label="*" onClick={() => handleAddNumber("*")}/>
         <Button label="x"/>
         </Row>
         <Row>
-        <Button label="7"/>
-        <Button label="8"/>
-        <Button label="9"/>
-        <Button label="+"/>
+        <Button label="7" onClick={() => handleAddNumber("7")}/>
+        <Button label="8" onClick={() => handleAddNumber("8")}/>
+        <Button label="9" onClick={() => handleAddNumber("9")}/>
+        <Button label="+" onClick={handleSomNumbers}/>
         </Row>
         <Row>
-        <Button label="4"/>
-        <Button label="5"/>
-        <Button label="6"/>
-        <Button label="."/>
+        <Button label="4" onClick={() => handleAddNumber("4")}/>
+        <Button label="5" onClick={() => handleAddNumber("5")}/>
+        <Button label="6" onClick={() => handleAddNumber("6")}/>
+        <Button label="." onClick={() => handleAddNumber(".")}/>
         </Row>
         <Row>
-        <Button label="1"/>
-        <Button label="2"/>
-        <Button label="3"/>
-        <Button label="="/>
+        <Button label="1" onClick={() => handleAddNumber("1")}/>
+        <Button label="2" onClick={() => handleAddNumber("2")}/>
+        <Button label="3" onClick={() => handleAddNumber("3")}/>
+        <Button label="=" onClick={() => handleAddNumber("=")}/>
         </Row>
       </Content>
     </Container>
